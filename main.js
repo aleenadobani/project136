a13d4a4f2281fcda9698eece1af5f757ea1292c2
aleenadobani <98019373+aleenadobani@users.noreply.github.com>
2022-09-22 18:57:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video = "";
+status = "";
+
+function preload (){
+}
+ function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+ }
+
+function draw() {
+    image(video,0,0,480,380);
+}
+
+function start ()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+        document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("model Loaded! ");
+    status = true;
+}
